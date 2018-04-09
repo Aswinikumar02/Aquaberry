075498e68cc4852f0a3bd8f12bd556c8c8ff5378
Aswini K (Digital) <AS351911@wipro.com>
2018-04-09 14:15:59 +0530

    back button in virtual fitting

--- client/components/VirtualFitting.jsx
+++ client/components/VirtualFitting.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Image } from "semantic-ui-react";
+import { Grid, Image, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import AppBar from './AppBar.jsx';
 
@@ -19,6 +19,20 @@ const VirtualFitting = () => {
           <Grid.Column width={16}>
             <center>
               <span style={{fontFamily:'Raleway',fontWeight:'bold',fontSize:'18px'}}>Click here to experiece the virtual fitting room.</span>
+            </center>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={16}>
+            <center>
+              <Link to='/wishList'>
+                <Button style={{
+                  backgroundColor: "#DF4B37",
+                  color: "white",
+                  boxShadow: "7px 16px 24px #eee",
+                  letterSpacing: "2px"
+                }} >Back</Button>
+              </Link>
             </center>
           </Grid.Column>
         </Grid.Row>
